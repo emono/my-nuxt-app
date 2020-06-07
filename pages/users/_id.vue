39db14ae206440ee1025f54aa05221f7ccfd4c3a
emono <ababababa0222@gmail.com>
2020-06-07 12:48:42 +0900

    first commit

---
+++ pages/users/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>UserID: {{ userId }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userId: this.$route.params.id,
+    };
+  },
+};
+</script>
